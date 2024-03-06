eb2b14d1ad16bef9502eb23dcf339552291f25eb
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-03-06 15:21:56 +0530

    added modified main file

--- main.py
+++ main.py
@@ -23,13 +23,11 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
-    
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
             scoreboard.game_over()
 
-    
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
